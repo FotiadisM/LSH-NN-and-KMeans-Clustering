5eea8f82c2a550e73baf0e168608729f7b3064f2
MichalisTatas <tatas.michalis@gmail.com>
2020-10-13 14:58:09 +0300

    created hash files

---
+++ src/hashTable.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "../include/hashTable.h"
+
+hashTable::hashTable()
+{
+
+}
+
+hashTable::~hashTable()
+{
+
+}
+
+void hashTable::insertItem()
+{
+
+}
+
+void hashTable::deleteItem() // prob not needed
+{
+
+}
+
+int hashTable::hashFunction()
+{
+
+}
+
+void hashTable::printHashTable()
+{
+    
+}
