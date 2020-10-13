a6d35f738f1aafdbebdb6df83a110fae4b108415
MichalisTatas <tatas.michalis@gmail.com>
2020-10-13 14:54:22 +0300

    added hash files

---
+++ include/hashTable.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <list>
+
+class hashTable
+{
+private:
+    int bucketNum;
+    std::list<int> * table;
+
+public:
+    hashTable();
+    ~hashTable();
+    void insertItem();
+    void deleteItem(); // prob not needed
+    int hashFunction();
+    void printHashTable();
+};
