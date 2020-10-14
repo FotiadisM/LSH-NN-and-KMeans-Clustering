3484cf07a3e09d30641516323f28f511bc569a12
Fotiadis Michail <fotiadis.michalis20@gmail.com>
2020-10-14 22:14:18 +0300

    Data::GetClosestNeighbors()

--- include/data.h
+++ include/data.h
@@ -7,9 +7,13 @@ class Data
 {
 private:
 public:
-    int n;                                  // number of data
-    int d;                                  // dimension
-    std::vector<std::vector<uint8_t>> data; // n * d array
+    int n; // number of data
+    int d; // dimension
+    std::vector<std::vector<uint8_t>> data;
 
     int InitMnistDataSet(std::ifstream &inputFile);
+
+    int EuclideanDistance(std::vector<uint8_t> &p1, std::vector<uint8_t> &p2);
+    int ManhattanDistance(const std::vector<uint8_t> &p1, const std::vector<uint8_t> &p2);
+    std::vector<uint8_t> GetClosestNeighbors(const std::vector<uint8_t> &point, const std::vector<std::vector<uint8_t>> &data, const int &N);
 };
--- src/data.cpp
+++ src/data.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <queue>
+#include <cmath>
+
 #include "../include/data.h"
 
 using namespace std;
@@ -60,4 +63,58 @@ int Data::InitMnistDataSet(std::ifstream &inputFile)
     }
 
     return 0;
+}
+
+int Data::EuclideanDistance(std::vector<uint8_t> &p1, std::vector<uint8_t> &p2)
+{
+    int d = 0;
+
+    for (size_t i = 0; i < p1.size(); i++)
+    {
+        d += pow(p2[i] - p1[i], 2);
+    }
+
+    return sqrt(d);
+}
+
+int Data::ManhattanDistance(const std::vector<uint8_t> &p1, const std::vector<uint8_t> &p2)
+{
+    int d = 0;
+
+    for (size_t i = 0; i < p1.size(); i++)
+    {
+        d += abs(p2[i] - p1[i]);
+    }
+
+    return d;
+}
+
+vector<uint8_t> Data::GetClosestNeighbors(const vector<uint8_t> &point, const vector<vector<uint8_t>> &data, const int &N)
+{
+    vector<uint8_t> result;
+    vector<pair<int, vector<uint8_t>>> costs;
+
+    for (auto &element : data)
+    {
+        costs.emplace_back(this->ManhattanDistance(point, element), element);
+    }
+
+    auto cmp = [](pair<int, vector<uint8_t>> left, pair<int, vector<uint8_t>> right) {
+        return left.first < right.first;
+    };
+    priority_queue<pair<int, vector<uint8_t>>, vector<pair<int, vector<uint8_t>>>, decltype(cmp)> q(cmp);
+
+    for (auto &element : costs)
+    {
+        q.push(element);
+    }
+
+    // pop from queue and return
+
+    // for (int i = 0; i < N; i++)
+    // {
+    //     result.push_back((vector<uint8_t>)q.pop().second);
+    // }
+
+    return result;
 }
